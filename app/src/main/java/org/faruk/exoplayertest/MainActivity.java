b3bb25d41454fc0cae5b44b336210ec0c2363a31
Faruk Yavuz <faruk@email.com>
2019-11-06 23:17:05 +0300

    Listener test

--- app/src/main/java/org/faruk/exoplayertest/MainActivity.java
+++ app/src/main/java/org/faruk/exoplayertest/MainActivity.java
@@ -1,22 +1,34 @@
 package org.faruk.exoplayertest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.audio.AudioAttributes;
+import com.google.android.exoplayer2.audio.AudioListener;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.source.dash.DashMediaSource;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.ui.PlayerControlView;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.VideoListener;
 
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -55,7 +67,103 @@ public class MainActivity extends AppCompatActivity {
         MediaSource contentMediaSource = buildMediaSource(Uri.parse(u));
 
         simpleExoPlayer.prepare(contentMediaSource);
-        simpleExoPlayer.setPlayWhenReady(true);
+        //simpleExoPlayer.setPlayWhenReady(true);
+
+        simpleExoPlayer.addListener(new Player.EventListener() {
+            @Override
+            public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+                Log.e("F40 ", "onTimelineChanged : " + timeline.toString());
+            }
+
+            @Override
+            public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+                Log.e("F40 ", "onTracksChanged : " + trackGroups.toString());
+            }
+
+            @Override
+            public void onLoadingChanged(boolean isLoading) {
+                Log.e("F40 ", "onLoadingChanged : " + isLoading);
+            }
+
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                Log.e("F40 ", "onPlayerStateChanged : " + playWhenReady);
+            }
+
+            @Override
+            public void onRepeatModeChanged(int repeatMode) {
+                Log.e("F40 ", "onRepeatModeChanged : " + repeatMode);
+            }
+
+            @Override
+            public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+                Log.e("F40 ", "onShuffleModeEnabledChanged : " + shuffleModeEnabled);
+            }
+
+            @Override
+            public void onPlayerError(ExoPlaybackException error) {
+                Log.e("F40 ", "onPlayerError : " + error.getMessage());
+            }
+
+            @Override
+            public void onPositionDiscontinuity(int reason) {
+                Log.e("F40 ", "onPositionDiscontinuity : " + reason);
+            }
+
+            @Override
+            public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+                Log.e("F40 ", "onPlaybackParametersChanged : " + playbackParameters);
+            }
+
+            @Override
+            public void onSeekProcessed() {
+                Log.e("F40 ", "onSeekProcessed");
+            }
+        });
+
+        simpleExoPlayer.addVideoListener(new VideoListener() {
+            @Override
+            public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+                Log.e("F40 ", "onVideoSizeChanged");
+            }
+
+            @Override
+            public void onSurfaceSizeChanged(int width, int height) {
+                Log.e("F40 ", "onSurfaceSizeChanged");
+            }
+
+            @Override
+            public void onRenderedFirstFrame() {
+                Log.e("F40 ", "onRenderedFirstFrame");
+            }
+        });
+
+        simpleExoPlayer.addAudioListener(new AudioListener() {
+            @Override
+            public void onAudioSessionId(int audioSessionId) {
+                Log.e("F40 ", "onAudioSessionId : " + audioSessionId);
+            }
+
+            @Override
+            public void onAudioAttributesChanged(AudioAttributes audioAttributes) {
+                Log.e("F40 ", "onAudioAttributesChanged : " + audioAttributes.toString());
+            }
+
+            @Override
+            public void onVolumeChanged(float volume) {
+                Log.e("F40 ", "onVolumeChanged : " + volume);
+            }
+        });
+
+        playerView.setControllerVisibilityListener(new PlayerControlView.VisibilityListener() {
+            @Override
+            public void onVisibilityChange(int visibility) {
+                Log.e("F40 ", "onVisibilityChange : " + visibility);
+            }
+        });
+
+        playerView.setControllerShowTimeoutMs(5000);
+
 
     }
 
